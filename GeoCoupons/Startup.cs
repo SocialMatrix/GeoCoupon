4d9305974149a19866dcda7ce5954275ab95d2ac
SocialMatrix <athekdi@asu.edu>
2014-06-15 17:37:09 -0700

    Initial Commit

---
+++ GeoCoupons/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GeoCoupons.Startup))]
+namespace GeoCoupons
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
